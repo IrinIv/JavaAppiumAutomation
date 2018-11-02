26bb4bfe94c0fb54d75fcd0ab3b9a41dc1701ac2
246_135 <My_email_adress@gmail.com>
2018-11-01 21:31:24 -0700

    Commit Test1

---
+++ src/MainClass.java
@@ -0,0 +1,16 @@
+public class MainClass {
+
+
+    public int getLocalNumber() {
+
+
+        return 14;
+
+    }
+
+
+
+
+
+}
+
---
+++ src/MainClassTest.java
@@ -0,0 +1,21 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class MainClassTest extends MainClass {
+
+    @Test
+    public void testGetLocalNumber() {
+
+     int a = this.getLocalNumber();
+     int expected = 14;
+
+
+        Assert.assertTrue( "This is wrong number", a == expected);
+
+
+    }
+
+
+}
